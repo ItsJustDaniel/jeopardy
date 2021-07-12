3797b7a67282327c854dd91002771e27442de155
Leinad <danielli12750@gmail.com>
2021-07-12 17:50:09 +1000

    added popup for questions

--- js/index.js
+++ js/index.js
@@ -1,29 +1,27 @@
-const onQuestionClick = () => {
-  location.href = "question";
-};
+// get elements
+const questionBtn = document.querySelectorAll(".jeopardy__button");
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("ansBtn");
+const span = document.getElementsByClassName("close")[0];
+const question__desc = document.getElementById("question__desc");
 
-// Get the modal
-var modal = document.getElementById("myModal");
+// scores
+let userScore = 0;
+let computer1Score = 0;
+let computer2Score = 0;
 
-// Get the button that opens the modal
-var btn = document.getElementById("ansBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-}
+questionBtn.forEach(
+  (i) =>
+    (i.onclick = (e) => {
+      console.log(e.target.innerText);
+      question__desc.innerHTML = `Topic 1 - ${e.target.innerText}`;
+      modal.style.display = "block";
+    })
+);
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
