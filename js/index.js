85b4aaec2a0c328d55365f36a1d60aa01633dea3
Leinad <danielli12750@gmail.com>
2021-07-13 06:38:37 +1000

    s

--- js/index.js
+++ js/index.js
@@ -25,12 +25,10 @@ let computer2Score = document.getElementById("jeopardy__compScore2");
 console.log(questionBtn);
 
 //active questions
-// let activeQuestions = [
-//   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-//   22, 23, 24,
-// ];
-
-let activeQuestions = [0, 1, 2];
+let activeQuestions = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+  22, 23, 24,
+];
 
 console.log(questions);
 
